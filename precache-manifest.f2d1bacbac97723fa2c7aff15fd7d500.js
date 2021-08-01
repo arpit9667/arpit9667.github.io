ac909f8790d904735728c4bb6b70dd1977a3132f
arpit9667 <arpitagrawal312@gmail.com>
2021-08-01 12:33:02 +0530

    Updates

---
+++ precache-manifest.f2d1bacbac97723fa2c7aff15fd7d500.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49559b135afb62ac23373636d7382495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f23c306421ac3a156a",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "2223068b73680568cac2",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "9f127dcb0516531504a0",
+    "url": "/static/css/main.fc5d9386.chunk.css"
+  },
+  {
+    "revision": "6bd56a87b87e22596e32",
+    "url": "/static/js/2.1426aa7d.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.1426aa7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f23c306421ac3a156a",
+    "url": "/static/js/3.3accb43f.chunk.js"
+  },
+  {
+    "revision": "2223068b73680568cac2",
+    "url": "/static/js/4.7a985149.chunk.js"
+  },
+  {
+    "revision": "9f127dcb0516531504a0",
+    "url": "/static/js/main.5c929014.chunk.js"
+  },
+  {
+    "revision": "958745dfb4a0ef2b35c6",
+    "url": "/static/js/runtime-main.b5e0e980.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "3952f41bd410b0f9b33c3242dc17ccda",
+    "url": "/static/media/coursera.3952f41b.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "67046ac38277017bd73ba11b646dea4b",
+    "url": "/static/media/gssoc.67046ac3.png"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
